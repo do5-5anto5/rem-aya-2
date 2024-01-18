86f4880aaebcfae7197dcbe2a706faf5967701ac
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-01-18 18:56:24 -0300

    Content Negotiation

---
+++ README.md
@@ -0,0 +1,1 @@
+API RESTful - Java 21 - Spring 3.2.1
--- rem_aya_2/src/main/java/br/com/rem_aya_2/controllers/PlantController.java
+++ rem_aya_2/src/main/java/br/com/rem_aya_2/controllers/PlantController.java
@@ -3,7 +3,6 @@ package br.com.rem_aya_2.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rem_aya_2.data_vo_v1.PlantVO;
 import br.com.rem_aya_2.services.PlantService;
+import br.com.rem_aya_2.util.MediaType;
 
 @RestController
 @RequestMapping("api/plant/v1")
@@ -24,25 +24,28 @@ public class PlantController {
 	@Autowired
 	PlantService service;
 	
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public List<PlantVO> findAll(){
 		return service.findAll();
 	}
 	
 	@GetMapping(value = "/{id}",
-				produces = MediaType.APPLICATION_JSON_VALUE)
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PlantVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 	
-	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
-				 consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PlantVO create(@RequestBody PlantVO plant) {
 		return service.create(plant);
 	}
 	
-	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
-			 	consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(
+			produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PlantVO update(@RequestBody PlantVO plant) {
 		return service.update(plant);
 	}
